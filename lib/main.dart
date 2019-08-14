5b7fa6d01f08a45cd1a35ca17850f6b4dc4daf80
cuba <ezeokecc@gmail.com>
2019-08-14 23:26:19 +0100

    I added the stateful class and stateless class, imports, scaffold, modularizations and widgets to flutter fashapp e-commerce project. happy hacking!

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,45 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Homepage(),
+  ));
+}
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text('Fashapp'),
+        backgroundColor: Colors.red,
+        actions: <Widget>[
+          new IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
+
+void secondExample() {
+  runApp(MaterialApp(
+    home: Center(child: Row(children: <Widget>[Text('two'), Text('three')])),
+  ));
+}
+
+void thirdExample() {
+  runApp(MaterialApp(
       home: GridView.count(
     crossAxisCount: 5,
     children: <Widget>[
@@ -22,10 +61,4 @@ void main() {
       Text('five'),
     ],
   )));
-}
-
-void second() {
-  runApp(MaterialApp(
-    home: Center(child: Row(children: <Widget>[Text('two'), Text('three')])),
-  ));
 }
